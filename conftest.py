fc9684e5f53cb83cab01b3f14e660c95f3422a17
lijiarui_vendor <lijiarui_vendor@sensetime.com>
2021-02-21 11:40:38 +0800

    init project

--- conftest.py
+++ conftest.py
@@ -32,14 +32,10 @@ def get_ws_url():
 
 @pytest.fixture(scope="function")
 def ws_connect_close(get_ws_url):
-
     websocket.enableTrace(True)  # 打开跟踪，查看日志
     ws = create_connection(get_ws_url)  # 创建连接
     ws.settimeout(10)  # 设置超时时间
-
     yield ws
-
-
     ws.close()
 
 
