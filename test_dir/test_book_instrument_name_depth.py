b604edf213732fedf0093af033674cfb1881d668
lijiarui_vendor <lijiarui_vendor@sensetime.com>
2021-02-21 11:25:52 +0800

    init project

--- test_dir/test_book_instrument_name_depth.py
+++ test_dir/test_book_instrument_name_depth.py
@@ -5,6 +5,7 @@ import websocket
 from common.excel_utiles import Excel
 from conftest import *
 
+
 def get_data():
     """
     读取参数化文件
@@ -16,37 +17,30 @@ def get_data():
     return testdatalist
 
 
-
-
-
-
 def test_send_info(ws_connect_close):
-        # 第一步：准备参数
-        params = {
-            "id": 11,
-            "method": "subscribe",
-            "params": {
-                "channels": ["book.ETH_CRO.150"]
-            },
-            "nonce": 1587523073344
-        }
-        expected = {'code': 0}
-        # 第二步：发送请求
-        ws_connect_close.send(json.dumps(params))
-        # 获取结果
-        ws_connect_close.recv()
-        result2 = ws_connect_close.recv()
+    # 第一步：准备参数
+    params = {
+        "id": 11,
+        "method": "subscribe",
+        "params": {
+            "channels": ["book.ETH_CRO.150"]
+        },
+        "nonce": 1587523073344
+    }
+    expected = {'code': 0}
+    # 第二步：发送请求
+    ws_connect_close.send(json.dumps(params))
+    # 获取结果
+    ws_connect_close.recv()
+    result2 = ws_connect_close.recv()
 
+    res2 = json.loads(result2)
 
-        res2 = json.loads(result2)
+    # 第三步：断言：
 
-        # 第三步：断言：
-
-        print("接收结果2：", res2)
-        assert expected['code']==res2['code']
-
+    print("接收结果2：", res2)
+    assert expected['code'] == res2['code']
 
 
 if __name__ == '__main__':
     pytest.main(["-s"])
-
